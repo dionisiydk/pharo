02f56c8f714a668ac07f076dfbecc6d1f938b604
Denis Kudriashov <dionisiydk@gmail.com>
2018-02-28 18:00:08 +0100

    SpecTableDataSource is introduced. It is do not depends on dast table data source. SpecTablePresenter should be configurared with instance of SpecTableDataSource subclasses. On morphic level this data source is wrapped by FTSpecTableDataSourceAdapter which simply delegate most  of messages to it. Example is not working based on SpecSimpleTableDataSource which is based on simple collection of items

--- bootstrap/scripts/runTests.sh
+++ bootstrap/scripts/runTests.sh
@@ -6,9 +6,6 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-# The first parameter is the architecture
-# The second parameter is the stage name
-
 ARCHFLAG=""
 if [ ${1} = "64" ]; then
     ARCHFLAG="64/"
@@ -28,7 +25,5 @@ CHANGES_FILE=$(find . -name Pharo7.0-${1}bit-*.changes)
 cp ${CACHE}/*.sources .
 mv $IMAGE_FILE Pharo.image
 mv $CHANGES_FILE Pharo.changes
-
-export PHARO_CI_TESTING_ENVIRONMENT=1
 					
-./pharo Pharo.image test --junit-xml-output --stage-name=${2} '.*'
+./pharo Pharo.image test --junit-xml-output '.*'
